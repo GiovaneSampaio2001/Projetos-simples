dd92319c9cc682fed7ff8a713e826293a7b8a4f9
Giovane Sampaio Sales De Jesus <85501896+GiovaneSampaio2001@users.noreply.github.com>
2022-01-27 14:16:11 -0300

    Novo Projeto

---
+++ pj15/script.js
@@ -0,0 +1,58 @@
+function PrimeiroFundo(){
+  document.body.style.background="Blue"
+}
+
+function SegundoFundo(){
+  document.body.style.background="red"
+}
+
+function TerceiroFundo(){
+  document.body.style.background="yellow"
+}
+
+function QuartoFundo(){
+  document.body.style.background="black"
+}
+
+function QuintoFundo(){
+  document.body.style.background="orange"
+}
+function SextoFundo(){
+  document.body.style.background="purple"
+}
+function SetimoFundo(){
+  document.body.style.background="pink"
+}
+function OitavoFundo(){
+  document.body.style.background="grey"
+}
+function NonoFundo(){
+  document.body.style.background="green"
+}
+function DecimoFundo(){
+  document.body.style.background="brown"
+}
+function DecimoPrimeiroFundo(){
+  document.body.style.background="white"
+}
+function DecimoSegundoFundo(){
+  document.body.style.background="lightblue"
+}
+function DecimoTerceiroFundo(){
+  document.body.style.background="darkblue"
+}
+function DecimoQuartoFundo(){
+  document.body.style.background="darkgreen"
+}
+function DecimoQuintoFundo(){
+  document.body.style.background="darkorange"
+}
+function DecimoSextoFundo(){
+  document.body.style.background="cyan"
+}
+function DecimoSetimoFundo(){
+  document.body.style.background="violet"
+}
+function DecimoOitavoFundo(){
+  document.body.style.background="lightgreen"
+}
