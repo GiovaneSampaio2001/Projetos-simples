6cb3bc2e5b011366254e606875844187fb1367ab
Giovane Sampaio Sales De Jesus <85501896+GiovaneSampaio2001@users.noreply.github.com>
2022-01-28 11:04:43 -0300

    novo Projeto

---
+++ pj16/script.js
@@ -0,0 +1,16 @@
+function Acender(){
+  var Luz1=document.querySelector('.luz')
+  Luz1.src="imagens/acessa.jpg"
+}
+
+
+function Desligar(){
+  var Luz2=document.querySelector(".luz")
+  Luz2.src="imagens/desligada.jpg"
+}
+
+
+function Quebra(){
+  var Luz3=document.querySelector(".luz")
+  Luz3.src="imagens/quebrada.jpg"
+}
