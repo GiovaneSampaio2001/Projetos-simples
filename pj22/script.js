d93b2619e8fa44592212183e2f3cfda928d8e786
Giovane Sampaio Sales De Jesus <85501896+GiovaneSampaio2001@users.noreply.github.com>
2022-02-09 14:58:03 -0300

    Novo Projeto Criado 22 Pj

---
+++ pj22/script.js
@@ -0,0 +1,37 @@
+var Resu=document.querySelector(".rsu")
+
+function Clicou1(){
+    Resu.innerHTML=""
+    for(let i=0;i<=100;i++){
+        Resu.innerHTML+= ` Número ${i} <br>`
+    }
+}
+
+function Clicou2(){
+    Resu.innerHTML=""
+    for(let i=200;i<=300;i++){
+        Resu.innerHTML+= ` Número ${i} <br>`
+    }
+}
+
+function Clicou3(){
+    Resu.innerHTML=""
+    for(let i=400;i<=500;i++){
+        Resu.innerHTML+= ` Número ${i} <br>`
+    }
+}
+
+
+function Clicou4(){
+    Resu.innerHTML=""
+    for(let i=600;i<=700;i++){
+        Resu.innerHTML+= ` Número ${i} <br>`
+    }
+}
+
+function Clicou5(){
+    Resu.innerHTML=""
+    for(let i=800;i<=1000;i++){
+        Resu.innerHTML+= ` Número ${i} <br>`
+    }
+}
